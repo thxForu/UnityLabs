60f852b1f4921a11c4f00e2f322d0266ae0cf59f
thxForu <xavavka12345@gmail.com>
2020-09-24 13:42:28 +0300

    Add Pendulum

---
+++ Assets/Scripts/PendulumMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PendulumMove : MonoBehaviour
+{
+    public Vector3 xAngle;
+
+    public float speed = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(transform.rotation.x);
+        transform.Rotate(xAngle,speed*Time.deltaTime);
+        
+        if (Mathf.Abs(transform.rotation.x) > 0.3)
+        {
+            xAngle *= -1;
+        } 
+    }
+    
+}
