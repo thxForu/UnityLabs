04dbcb014ad6609d1999d4813e066e4e3d6966f9
thxForu <xavavka12345@gmail.com>
2020-09-24 15:22:07 +0300

    Add Ball

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+
+{
+    public float thrust = 1.0f;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        _rigidbody.AddForce(-transform.forward * thrust);
+        _rigidbody.useGravity = true;
+    }
+}
