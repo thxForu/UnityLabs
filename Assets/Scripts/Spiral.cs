db034e420f45285fafdfd98a186e26d6686c5a21
thxForu <xavavka12345@gmail.com>
2020-09-24 15:21:49 +0300

    Add Ball

---
+++ Assets/Scripts/Spiral.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spiral : MonoBehaviour
+{
+    public float angle;
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        angle += 0.25f;
+        transform.Rotate(Vector3.up, angle * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
